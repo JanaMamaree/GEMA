082b9590de8ff42dbd8eb22ac9529738e5b4b6fa
danielerogano <danielerogano@gmail.com>
2025-06-27 12:44:14 +0200

    app url added

--- GEMA/urls.py
+++ GEMA/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapp.urls')),
-    path('', include('myapp.urls')),    
+    path('app/', include('myapp.urls')),
 ]
 
 # Serve static files in development
