0f30c6a81c5bfaf1a9f6fd70ecc73b49c59bc8bb
JanaMamaree <jmamaree@gmail.com>
2025-07-01 09:31:00 +0200

    fix problem of images

--- GEMA/settings.py
+++ GEMA/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qwldxti^jt6vaenami&jh(^(^!7w6(qs@w=s#fni%o+wm^_jl+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = ["*"]
 ALLOWED_HOSTS = ["gemact.somos.srl", '127.0.0.1', 'localhost', '*']
@@ -167,9 +167,8 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-FORCE_SCRIPT_NAME = "/app" #should be removed 
-STATIC_URL = "/app/static/"
-MEDIA_URL = "/app/media/"
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'myapp', 'static'),
